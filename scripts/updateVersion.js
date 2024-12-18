2766cfda4678fd3566ac4515da8142e36694f057
Amith-B <amithbr6@gmail.com>
2024-12-18 19:44:12 +0530

    initial setup with react

---
+++ scripts/updateVersion.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+const packageJsonPath = path.join(__dirname, "..", "package.json");
+const manifestJsonPath = path.join(__dirname, "..", "public", "manifest.json");
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+const manifestJson = JSON.parse(fs.readFileSync(manifestJsonPath, "utf8"));
+
+manifestJson.version = packageJson.version;
+
+fs.writeFileSync(
+  manifestJsonPath,
+  JSON.stringify(manifestJson, null, 2),
+  "utf8"
+);
+
+console.log(`Updated manifest.json version to ${packageJson.version}`);
