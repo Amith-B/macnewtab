075d4c7f74463e3e846880a656bdcc779e1866f2
Amith-B <amithbr6@gmail.com>
2025-01-10 00:19:58 +0530

    feat: add voice search functionality

--- src/widgets/search/Search.tsx
+++ src/widgets/search/Search.tsx
@@ -1,10 +1,29 @@
-import React, { useContext, useState } from "react";
+import React, {
+  ChangeEvent,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { ReactComponent as SearchIcon } from "./search-icon.svg";
 import "./Search.css";
 import { searchEngineList } from "../../static/searchEngine";
 import { AppContext } from "../../context/provider";
 import { translation } from "../../locale/languages";
 import { ReactComponent as VoiceSearch } from "../../assets/voice.svg";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const triggerSearch = (searchString: string, selectedSearchEngine: string) => {
+  const searchSelection = searchEngineList.find(
+    (item) => item.key === selectedSearchEngine
+  );
+
+  if (searchString.trim() !== "") {
+    searchSelection?.searchFunction(searchString);
+  }
+};
 
 export default function Search({
   selectedSearchEngine,
@@ -13,32 +32,73 @@ export default function Search({
 }) {
   const [searchString, setSearchString] = useState("");
   const { locale } = useContext(AppContext);
-  const [animate, setAnimate] = useState(false);
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const listenerRef = useRef("");
+  const hiddenButtonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (listening && listenerRef.current !== "listening") {
+      listenerRef.current = "listening";
+    }
+
+    if (!listening && listenerRef.current !== "abort") {
+      hiddenButtonRef.current?.click();
+    }
+  }, [listening]);
 
   const handleKeyDown = (evt: React.KeyboardEvent) => {
     if (evt.key === "Enter") {
-      const searchSelection = searchEngineList.find(
-        (item) => item.key === selectedSearchEngine
-      );
-
-      if (searchString.trim() !== "") {
-        searchSelection?.searchFunction(searchString);
-      }
+      triggerSearch(searchString, selectedSearchEngine);
     }
+  };
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    resetTranscript();
+    setSearchString(e.target.value);
+  };
+
+  const handleVoiceSearch = () => {
+    resetTranscript();
+    setSearchString("");
+    if (listening) {
+      SpeechRecognition.abortListening();
+      listenerRef.current = "abort";
+      return;
+    }
+    SpeechRecognition.startListening();
+  };
+
+  const triggerVoiceSearch = () => {
+    triggerSearch(transcript, selectedSearchEngine);
   };
 
   return (
     <div className="search__container">
       <SearchIcon className="search-icon__container" />
       <input
-        value={searchString}
-        onChange={(e) => setSearchString(e.target.value)}
+        value={transcript || searchString}
+        onChange={handleInput}
         placeholder={translation[locale]["search"]}
         onKeyDown={handleKeyDown}
       />
-      <button className="voice-search" onClick={() => setAnimate(!animate)}>
-        <VoiceSearch className={animate ? "animate" : ""} />
-      </button>
+      {browserSupportsSpeechRecognition && (
+        <>
+          <button className="voice-search" onClick={handleVoiceSearch}>
+            <VoiceSearch className={listening ? "animate" : ""} />
+          </button>
+          <button
+            ref={hiddenButtonRef}
+            className="hidden-voice-search-button"
+            onClick={triggerVoiceSearch}
+          ></button>
+        </>
+      )}
     </div>
   );
 }
