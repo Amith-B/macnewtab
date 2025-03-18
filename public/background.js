241eed6de71f49bf301e9a91c4442f607e5356bb
Amith-B <37371517+Amith-B@users.noreply.github.com>
2025-03-18 15:23:46 +0530

    refactor: prettify

--- public/background.js
+++ public/background.js
@@ -1,6 +1,5 @@
 /* global chrome */
 chrome.runtime.onInstalled.addListener(() => {
-    // Open a new empty tab after the extension is installed
-    chrome.tabs.create({});
-  });
-  
+  // Open a new empty tab after the extension is installed
+  chrome.tabs.create({});
+});
