2de6bbcb805a85ab19c8929da06c34578d849984
Amith-B <amithbr6@gmail.com>
2026-03-01 22:58:51 +0530

    fix: expand interactive canvas width and hight to 100%

--- src/components/wallpaper/InteractiveWallpaper.tsx
+++ src/components/wallpaper/InteractiveWallpaper.tsx
@@ -58,8 +58,8 @@ const InteractiveWallpaper: React.FC<InteractiveWallpaperProps> = memo(
           position: "fixed",
           top: 0,
           left: 0,
-          width: "100vw",
-          height: "100vh",
+          width: "100%",
+          height: "100%",
           zIndex: -1,
           overflow: "hidden",
         }}
